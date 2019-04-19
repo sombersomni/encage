e0edb549d6a86dd06576ab0d2343d285c6a0db6f
somberSomni <kartunehustla@gmail.com>
2019-04-19 00:49:54 -0500

    private variables are now functional. work on initialized functions

--- src/index.js
+++ src/index.js
@@ -1,128 +1,100 @@
 function encage(Parent) {
-  //creates the static holder variables for main Class
-  if (!(Parent instanceof Function) && !(Parent.isPrototypeOf(Object)))
-    throw new TypeError('Must use a constructor Function or Object');
-
-  const Root = {};
-  if (Parent.isPrototypeOf(Object)) {
-    Root = Object.freeze(Parent);
-  } else {
-    try {
-      Root = Object.assign({}, new Parent());
-      console.log(Root);
-    } catch (err) {
-      console.warn("make sure to use a constructor function")
+  const isObject = (typeof Parent === 'object' && Parent.constructor === Object);
+  if (Parent && Parent instanceof Function || isObject) {
+    let Root = {};
+    if (isObject) {
+      Root = Object.freeze(Parent);
+    } else {
+      try {
+        Root = Object.assign({}, new Parent());
+      } catch (err) {
+        console.warn("make sure to use a constructor function")
+      }
     }
-  }
-  function Encaged() {
-    if (Root instanceof Function) {
-    } else if (Object.prototype.isPrototypeOf(Root) && Root.static != undefined) {
-      for (let key in Root.static) {
-        if (!(Root.static[key] instanceof Function)) {
-          this[key] = Root.static[key]
+    function Encaged() {
+      if (Root instanceof Function) {
+      } else if (Object.prototype.isPrototypeOf(Root) && Root.static != undefined) {
+        for (let key in Root.static) {
+          if (!(Root.static[key] instanceof Function)) {
+            this[key] = Root.static[key]
+          }
         }
       }
     }
-  }
-  Encaged.prototype = {
-    create: function () {
-      //setup for public variables
-      const publicProps = {};
-      const privateProps = {};
-      let filteredOutArgsPublic = [];
-      let filteredOutArgsPrivate = [];
-      if (arguments.length == 1 && arguments[0] instanceof Object && Object.keys(arguments[0]).length > 0) {
-        filteredOutArgsPublic = Root.public != undefined? Object.keys(Root.public).filter(prop => !arguments[0].hasOwnProperty(prop)) : [];
-        filteredOutArgsPrivate = Root.private != undefined? Object.keys(Root.private).filter(prop => !arguments[0].hasOwnProperty(prop)) : [];
-        Object.keys(arguments[0]).forEach(prop => {
-          if (Root.public != undefined && Root.public.hasOwnProperty(prop)) {
-            const value = arguments[0][prop];
-            publicProps[prop] = {
-              value,
-              writeable: true,
-              enumerable: true
-            }
-          } else if (Root.private != undefined && Root.private.hasOwnProperty(prop)) {
-            const value = arguments[0][prop];
-            privateProps[prop] = value;
-          }
-        });
-      }
-      //setup for public methods
-      let _private = { job: 'sports' };
-      //creates a new instance to configure before returning to user
-      const initialize = () => {
-        let newInst = Object.create({}, publicProps);
-        if( Root.public != undefined) {
-          filteredOutArgsPublic.forEach(prop => {
-            if (Root.public[prop] instanceof Function) {
-              newInst[prop] = function () {
-                return Root.public[prop].apply(
-                  Object.assign(
-                    {}, newInst,
-                    Root.static != undefined ? { static: this } : {},
-                    Root.private != undefined ? { private: Object.assign(_private) } : {}
-                  ), arguments);
+    Encaged.prototype = {
+      create: function () {
+        //setup for public variables
+        //assign arguments from create to public and private set variables
+        if (arguments.length == 1 && arguments[0] instanceof Object && Object.keys(arguments[0]).length > 0) {
+          Root
+        }
+        const publicProps = {};
+        let filteredOutArgsPublic = [];
+        if (arguments.length == 1 && arguments[0] instanceof Object && Object.keys(arguments[0]).length > 0) {
+          filteredOutArgsPublic = Root.public != undefined ? Object.keys(Root.public).filter(prop => !arguments[0].hasOwnProperty(prop)) : [];
+          Object.keys(arguments[0]).forEach(prop => {
+            //assigns class arguments to public vars
+            if (Root.public != undefined && Root.public.hasOwnProperty(prop)) {
+              const value = arguments[0][prop];
+              publicProps[prop] = {
+                value,
+                writeable: true,
+                enumerable: true
               }
+              //assigns class arguments to public vars
+            } else if (Root.private != undefined && Root.private.hasOwnProperty(prop)) {
+              const value = arguments[0][prop];
+              Root.private[prop] = value;
             }
           });
         }
-        //creates copy of instance so we don't add static or private variables
-        const instCopy = Object.assign({}, newInst, Root.static != undefined ? { static: this } : {});
-        if(Root._init != undefined) {
-          Object.values(Root._init).forEach(fn => {
-            fn.call(instCopy);
-          })
+        //setup for public methods
+        let _private = Object.assign({}, Root.private);
+        //creates a new instance to configure before returning to user
+        const initialize = () => {
+          let newInst = Object.create({}, publicProps);
+          //maps all functions to instance and private/static variables using apply
+          if (_private) {
+            for (let prop in _private) {
+              if (_private[prop] instanceof Function) {
+                const tempFn = _private[prop];
+                _private[prop] = function () {
+                  return tempFn.apply(Object.assign({}, newInst,
+                    Root.static != undefined ? { static: this } : {}, { private: Object.assign(_private) }), arguments);
+                }
+              }
+            }
+          }
+          if (Root.public) {
+            filteredOutArgsPublic.forEach(prop => {
+              if (Root.public[prop] instanceof Function) {
+                newInst[prop] = function () {
+                  return Root.public[prop].apply(Object.assign({}, newInst,
+                    Root.static != undefined ? { static: this } : {},
+                    _private != undefined ? { private: Object.assign(_private) } : {}), arguments);
+                }
+              }
+            });
+          }
+          //creates copy of instance so we don't add static or private variables
+          if (Root._init) {
+            for(prop in Root._init) {
+              Root._init[prop].call(Object.assign({}, newInst,
+                Root.static != undefined ? { static: this } : {},
+                _private != undefined ? { private: Object.assign(_private) } : {}));
+            }
+          }
+          return newInst;
         }
-        return newInst;
+        return initialize();
+
       }
-      return initialize();
-
     }
+    const inst = new Encaged();
+    return inst;
+  } else {
+    throw new TypeError('Must use a constructor Function or Object');
   }
-  const inst = new Encaged();
-  return inst;
 }
-
-//testing
-const Person = {
-  static: {
-    numOfPeople: 0,
-    allPeople: []
-  },
-  public: {
-    name: 'Default Name',
-    age: 214,
-    hobbies: ['programming', 'math'],
-    getJob: function () {
-      return this.private.job;
-    },
-    setJob: function (job) {
-      this.private.job = job;
-    },
-  },
-  private: {
-    job: 'Podcaster',
-  },
-  _init: {
-    addPerson: function () {
-      this.static.numOfPeople++;
-      this.static.allPeople.push(this.name);
-    }
-  },
-}
-const EncagedPeople = encage(function () { });
-console.log(EncagedPeople);
-const xavier = EncagedPeople.create({ name: "Xavier", age: 10, job: 'sports' });
-//if you dont use the object syntax above, make sure to
-//add arguments in same order as you placed them in public
-//const stark = EncagedPeople.create("Stark");
-console.log(xavier);
-// console.log(xavier.getJob());
-// xavier.setJob('skater');
-// console.log("new job", xavier.getJob());
-// xavier.setJob('racer');
-// console.log(xavier.getJob());
-// console.log(EncagedPeople);
 
 module.exports = encage;
--- test/encage.test.js
+++ test/encage.test.js
@@ -1,12 +1,90 @@
 var chai = require('chai');
 var encage = require('../src/index.js');
 var assert = chai.assert;
+var expect = chai.expect;
 
-describe('#encage', function() {
-    it('throws error if encage takes any but Function or Object', function() {
+let BankAccount;
+describe('#encage', function () {
+    it('throws error if encage takes any but Function or Object', function () {
         assert.throws(encage.bind(null, "config"), 'Must use a constructor Function or Object');
         assert.throws(encage.bind(null, 3), 'Must use a constructor Function or Object');
         assert.throws(encage.bind(null, true), 'Must use a constructor Function or Object');
         assert.throws(encage.bind(null, []), 'Must use a constructor Function or Object');
     })
+    describe('#create', function () {
+        before(function () {
+            BankAccount = {
+                _init: {
+                    addClient: function () {
+                        this.static.numOfAccounts++;
+                        this.static.clients.push(this.name);
+                    }
+                },
+                static: {
+                    numOfAccounts: 0,
+                    clients: [],
+                },
+                public: {
+                    name: '',
+                    bankName: 'regions',
+                    setPassword: function(password) {
+                        this.private.password = password;
+                    },
+                    getBalance: function () {
+                        return this.private.balance;
+                    },
+                    widthdraw: function (password, amount) {
+                        if (this.private.checkPassword(password)) {
+                            this.private.reduceBalance(amount);
+                            console.log("Account balance is : " + this.private.balance)
+                            return amount;
+                        }
+                    },
+                    deposit: function (password, amount) {
+                        if (this.private.checkPassword(password)) {
+                            this.private.addBalance(amount);
+                            console.log("Account balance is : " + this.private.balance)
+                        }
+                    },
+                },
+                private: {
+                    accountNumber: 0,
+                    balance: 0,
+                    password: "test",
+                    checkPassword: function (password) {
+                        return password === this.private.password
+                    },
+                    reduceBalance: function (amount) {
+                        this.private.balance -= amount;
+                    },
+                    addBalance: function (amount) {
+                        this.private.balance += amount;
+                    }
+                }
+            }
+
+        })
+        it('keeps track of 5 instances using static variables', function () {
+            const eBankAccount = encage(BankAccount);
+            const account = eBankAccount.create({ name: 'Xavier', bankName: 'Regions', accountNumber: 10204343 });
+            const account2 = eBankAccount.create({ name: 'Korey', bankName: 'Regions', accountNumber: 43462345 });
+            const account3 = eBankAccount.create({ name: 'Martin', bankName: 'Bank Of America', accountNumber: 234323324 });
+            const account4 = eBankAccount.create({ name: 'Gurjot', bankName: 'Credit Union', accountNumber: 212349021 });
+            const account5 = eBankAccount.create({ name: 'Scarlett', bankName: 'Chase', accountNumber: 23423534 });
+            expect(eBankAccount.clients).to.be.an.instanceof(Array);
+            expect(eBankAccount.clients.length).to.equal(5);
+            expect(eBankAccount.numOfAccounts).to.equal(5);
+
+        })
+        it('can change private variables while keeping them unreachable', function () {
+            const eBankAccount = encage(BankAccount);
+            const account = eBankAccount.create({ name: 'Xavier', bankName: 'Regions', accountNumber: 10204343 });
+            expect(account.balance).to.be.undefined;
+            expect(account.password).to.be.undefined;
+            expect(account.getBalance()).to.equal(0);
+            account.setPassword("marvel");
+            account.deposit("marvel", 1000);
+            expect(account.getBalance()).to.equal(1000);
+        })
+    });
 });
